2b4cf7a6086d4ec762a46181110d23ce959d75fd
yeswanthkanakala07 <yeswanth2001kanakala@gmail.com>
2024-11-28 15:32:19 -0600

    Update views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated,AllowAny
-from .serializers import Userserializer
+from .serializers import UserSerializer
 
 # Create your views here.
 
 class UserCreateView(generics.CreateAPIView):
     queryset = User.objects.all()
-    serializer_class = Userserializer
+    serializer_class = UserSerializer
     permission_classes = [AllowAny]
