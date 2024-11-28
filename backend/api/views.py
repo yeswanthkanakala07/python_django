90280198d6a4b39f625c8a3ad8ccc039efdf1cca
yeswanthkanakala07 <yeswanth2001kanakala@gmail.com>
2024-11-28 15:12:14 -0600

    Update views.py
    View implementation for the user in the user registration page.

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated,AllowAny
+from .serializers import Userserializer
 
 # Create your views here.
+
+class UserCreateView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = Userserializer
+    permission_classes = [AllowAny]
